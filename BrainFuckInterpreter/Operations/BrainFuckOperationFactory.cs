fc954c00257f20a2bde535c1f53c309818cfb8c0
Anjou <krzysztof.jan.kroczak@gmail.com>
2016-11-12 21:58:24 +0100

    Added operations

---
+++ BrainFuckInterpreter/Operations/BrainFuckOperationFactory.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BrainFuckInterpreter.Operations
+{
+	class BrainFuckOperationFactory
+	{
+		public static IBrainFuckOperation[] GetAllOperations()
+		{
+			return new IBrainFuckOperation[]
+			{
+				new IncrementPointer(),
+				new DecrementPointer(),
+				new IncrementIndicatedCellValue(),
+				new DecrementIndicatedCellValue(),
+				new GetByteValueToIndicatedCell(),
+				new PrintIndicatedCellValue(),
+				new Loop()
+			};
+		}
+	}
+}
---
+++ BrainFuckInterpreter/Program.cs
@@ -0,0 +1,28 @@
+﻿using BrainFuckInterpreter.Operations;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BrainFuckInterpreter
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var operations = BrainFuckOperationFactory.GetAllOperations();
+			int i = 0;
+			while(Console.ReadLine()!="quit")
+			{
+				var left = Console.CursorLeft;
+				var top = Console.CursorTop;
+				i++;
+				Console.SetCursorPosition(10, 0);
+				Console.CursorTop = Console.CursorTop>0 ? Console.CursorTop-1 : 0;
+				Console.WriteLine(string.Format("\b\b\rdsadasd {0}", i));
+				Console.SetCursorPosition(left, top);
+			}
+		}
+	}
+}
